8e6135db4dcb0187f56e4da119d8dd482165d991
Alvaro del Castillo San Felix <alvarodelcastillo@gmail.com>
2021-01-25 08:13:32 +0100

    Sender also receives messages

--- microstreams/message-sender/src/main/java/acs/me/microstreams/sender/SenderApplication.java
+++ microstreams/message-sender/src/main/java/acs/me/microstreams/sender/SenderApplication.java
@@ -1,14 +1,42 @@
 package acs.me.microstreams.sender;
 
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import acs.me.microstreams.sender.entities.Message;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
 public class SenderApplication {
 
+	private final String[] msgs = {"message1", "message2", "message3", "message4"};
+
+	private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(SenderApplication.class);
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(SenderApplication.class, args);
+	}
+
+	@Bean
+	public Supplier<Message> sendMessages() {
+		return () -> {
+			Message message = new Message();
+			message.setMessage(this.msgs[new Random().nextInt(this.msgs.length)]);
+			return message;
+		};
+	}
+
+	@Bean
+	public Consumer<Message> logUserMessage() {
+		return message -> {
+			LOGGER.info(message.getMessage());
+		};
 	}
 
 }
